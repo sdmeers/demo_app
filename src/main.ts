d1e512ca3ae660e676a474e9055c17d92140399d
Steven Meers <stevenmeers@gmail.com>
2025-07-28 21:24:51 +0100

    Fixed app launching in debug mode

--- src/main.ts
+++ src/main.ts
@@ -51,8 +51,7 @@ const createWindow = () => {
   // Load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
-    // Open the DevTools in development.
-    mainWindow.webContents.openDevTools();
+    
   } else {
     mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
   }
